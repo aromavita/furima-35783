3186a4e29b141d8848791b84fe291b8aae34f433
aromavita <ysk6417@gmail.com>
2021-09-11 19:29:36 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   with_options presence: true do
-    validates :nickname, presence:true
-    validates :family_name, :first_name, :family_name_kana, :first_name_kana, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "お名前は全角で入力してください"}
-    validates :birthday, presence:true
-
-    
+    validates :nickname, presence: true
+    validates :family_name, :first_name, :family_name_kana, :first_name_kana,
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'お名前は全角で入力してください' }
+    validates :birthday, presence: true
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,24 +6,23 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-
     context '新規登録できるとき' do
-
       it 'ニックネーム、メールアドレス、パスワード、お名前、お名前(カナ)、生年月日が存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
 
     context '新規登録できないとき' do
-
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
 
       it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
 
       it '重複したメールアドレスがある場合は登録できない' do
@@ -48,25 +47,17 @@ RSpec.describe User, type: :model do
         @user.valid?
       end
 
-      it 'パスワードとパスワード(確認)が異なっている場合は登録できない' do
-        
-      end
-
       it 'お名前は全角でないと登録できない' do
-        
+        @user.family_name = 'ﾔﾏﾀﾞ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Family name お名前は全角で入力してください')
       end
 
       it '生年月日が空欄の場合は登録できない' do
         @user.birthday = ''
         @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
-
-      it '' do
-        
-      end
-
-
-
     end
   end
 end
