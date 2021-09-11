42df718c4b528ac02b68883a85a3ac1eb377b382
aromavita <ysk6417@gmail.com>
2021-09-11 15:37:40 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  with_options presence: true do
+    validates :nickname, presence:true
+    validates :family_name, :first_name, :family_name_kana, :first_name_kana, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "お名前は全角で入力してください"}
+
+
+    
+  end
 end
